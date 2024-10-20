1ffc45a87502b58791d6bfd605aa676ab79fd6d9
dominickstephens <dominickdstephens@gmail.com>
2024-10-20 15:40:00 +0100

    Added subtract tests

--- src/test.cpp
+++ src/test.cpp
@@ -12,6 +12,10 @@ int add(int a, int b) {
     return a + b;
 }
 
+int subtract(int a, int b) {
+    return a - b;
+}
+
 // Simple test for the add function
 void test_add() {
     if (add(2, 3) != 5) {
@@ -27,7 +31,23 @@ void test_add() {
     }
 }
 
+//test subtraction function
+void test_subtract() {
+    if (subtract(2, 3) != -1) {
+        cerr << "Test failed: subtract(2, 3) != -1" << std::endl;
+    } else {
+        cout << "Test passed: subtract(2, 3) == -1" << std::endl;
+    }
+
+    if (subtract(-1, 1) != -2) {
+        cerr << "Test failed: subtract(-1, 1) != -2" << std::endl;
+    } else {
+        cout << "Test passed: subtract(-1, 1) == -2" << std::endl;
+    }
+}
+
 int main() {
     test_add();  // Run the tests
+    test_subtract();
     return 0;
 }
