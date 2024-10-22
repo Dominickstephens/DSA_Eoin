ec1328544a94ae6ce457d0c4b51cbb32f14126a8
AmyRoys <114415869+AmyRoys@users.noreply.github.com>
2024-10-22 15:27:42 +0100

    Add implementation of set

---
+++ include/Set.h
@@ -0,0 +1,27 @@
+#ifndef SET_H
+#define SET_H
+
+#include <vector>
+#include <algorithm>
+
+template <typename T>
+class Set {
+public:
+    Set() = default;
+
+    void insert(const T& value);
+    bool contains(const T& value) const;
+    void erase(const T& value);
+    std::vector<T> toVector() const;
+
+    Set<T> setIntersection(const Set<T>& other) const;
+    Set<T> setUnion(const Set<T>& other) const;
+    Set<T> setDifference(const Set<T>& other) const;
+
+private:
+    std::vector<T> elements;
+};
+
+#include "../src/Set.tpp"
+
+#endif // SET_H
