53cb4a072ee9eb193fbe8b377993c40307eb8f5d
Rosie-Kennelly <89597390+Rosie-Kennelly@users.noreply.github.com>
2024-10-22 21:43:15 +0100

    Update comments for test_autocomplete_handler.cpp

--- tests/test_autocomplete_handler.cpp
+++ tests/test_autocomplete_handler.cpp
@@ -2,68 +2,65 @@
 #include "../include/AutocompleteHandler.h"
 #include "../include/Trie.h"
 
-// Test for toLowerCase function
+// toLowerCase
 TEST(AutocompleteHandlerTests, ToLowerCase) {
     std::string input = "HeLLo";
     std::string expected = "hello";
     ASSERT_EQ(toLowerCase(input), expected);
 }
 
-// Test for getLastWord function
+// getLastWord
 TEST(AutocompleteHandlerTests, GetLastWord) {
     std::string input = "This is a test";
     std::string expected = "test";
     ASSERT_EQ(getLastWord(input), expected);
 }
 
-// Test for replaceLastWord function
+// replaceLastWord
 TEST(AutocompleteHandlerTests, ReplaceLastWord) {
     std::string input = "This is a test";
     std::string suggestion = "success";
     std::string expected = "This is a success";
     ASSERT_EQ(replaceLastWord(input, suggestion), expected);
 
-    // Test for single word replacement
+    // single word replacement
     input = "test";
     expected = "success";
     ASSERT_EQ(replaceLastWord(input, suggestion), expected);
 }
 
-// Test for loadBookTitles function
+// loadBookTitles 
 TEST(AutocompleteHandlerTests, LoadBookTitles) {
     Trie<char> trie;
     documentIndex<std::string, vectorClass<IndexEntry>> index;
 
-    // Add some dummy titles to the index
     index.insert("Harry Potter", {});
     index.insert("The Hobbit", {});
     index.insert("The Lord of the Rings", {});
 
     loadBookTitles(trie, index);
 
-    // Test if the titles are in the Trie
     ASSERT_TRUE(trie.search("Harry Potter"));
     ASSERT_TRUE(trie.search("The Hobbit"));
     ASSERT_TRUE(trie.search("The Lord of the Rings"));
 }
 
-// Test for autocomplete using the Trie
+// autocomplete using the Trie
 TEST(AutocompleteHandlerTests, AutocompleteSuggestions) {
     Trie<char> trie;
     documentIndex<std::string, vectorClass<IndexEntry>> index;
 
-    // Add some titles to the index
     index.insert("The Lord of the Rings", {});
     index.insert("The Hobbit", {});
     index.insert("The Silmarillion", {});
 
     loadBookTitles(trie, index);
 
-    // Autocomplete "The"
+    // autocomplete "The"
     std::string input = "The";
     std::vector<std::string> suggestions = trie.autocomplete(toLowerCase(input));
 
-    ASSERT_EQ(suggestions.size(), 3);  // Should return all three titles
+    ASSERT_EQ(suggestions.size(), 3);  
     ASSERT_EQ(suggestions[0], "the hobbit");
     ASSERT_EQ(suggestions[1], "the lord of the rings");
     ASSERT_EQ(suggestions[2], "the silmarillion");
